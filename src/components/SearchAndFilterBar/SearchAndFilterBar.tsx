1c1831cc5cb4412e6b43915c821aca7cfd43c7ee
Amanda Ogletree <amandaogletree19@gmail.com>
2026-01-05 11:28:20 -0500

    Added Button for Search bar in case users aren't internet savy to press enter. Ease of use/ Quality of life update.

--- src/components/SearchAndFilterBar/SearchAndFilterBar.tsx
+++ src/components/SearchAndFilterBar/SearchAndFilterBar.tsx
@@ -7,30 +7,37 @@ export default function SearchAndFilterBar() {
   const { theme } = useThemeContext();
 
   //State and state change on new Query Typed in Searchbar
-  const [query, setQuery] = useState('')
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-      const newValue = event.target.value
-      setQuery(newValue)
-  }
+  const [query, setQuery] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setQuery(newValue);
+  };
 
-  //Set new fitler upon enter of search bar or change of select element
-  const {setFilter} = useFilterContext()
-  const handleFilterChangeOnSearch = (event: React.KeyboardEvent) =>{
-    if(event.key === 'Enter'){
-      setFilter(query)
+  //Set new fitler upon enter of search bar, click of search button, or change of select element
+  const { setFilter } = useFilterContext();
+  const handleFilterChangeOnSearch = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      setFilter(query);
     }
-  }
-  const handleFilterChangeOnSelect = (event: React.ChangeEvent<HTMLSelectElement>)=>{
-    const newValue = event.target.value
-      setFilter(newValue)
-  }
-  
-   //show all when blank and unfocused
-  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) =>{
-    if(query === ''){
-      setFilter(query)
+  };
+  const handleFilterOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setFilter(query);
+  };
+  const handleFilterChangeOnSelect = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newValue = event.target.value;
+    setFilter(newValue);
+  };
+
+  //show all when blank and unfocused
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (query === "") {
+      setFilter(query);
     }
-  }
+  };
 
   return (
     <search
@@ -41,27 +48,36 @@ export default function SearchAndFilterBar() {
         id="country-search-bar-container"
         className="col-md-10 d-flex flex-column"
       >
-        <input
-          onChange={handleChange}
-          onKeyUp={handleFilterChangeOnSearch}
-          onBlur={handleBlur}
-          id="country-search-bar"
-          className={`shadow-sm ${theme === 'light' ? 'dark' : 'light'}`}
-          type="search"
-          name="search"
-          placeholder="Search for a country... Try 'Cambodia!'"
-          minLength={3}
-          pattern="[a-z A-Z]+"
-          spellCheck="true"
-          aria-label="Search for a country by name."
-        />
+        <div className="text-start">
+          <input
+            onChange={handleChange}
+            onKeyUp={handleFilterChangeOnSearch}
+            onBlur={handleBlur}
+            id="country-search-bar"
+            className={`shadow-sm ${theme === "light" ? "dark" : "light"}`}
+            type="search"
+            name="search"
+            placeholder="Search for a country... Try 'Cambodia!'"
+            minLength={3}
+            pattern="[a-z A-Z]+"
+            spellCheck="true"
+            aria-label="Search for a country by name."
+          />
+          <button
+            onClick={handleFilterOnClick}
+            id="country-search-bar-button"
+            className={`ms-2 ${theme === "light" ? "dark" : "light"}`}
+          >
+            Search by Name
+          </button>
+        </div>
         <span id="search-error"></span>
       </div>
-      <div id="region-select-container" className="col-md-2">
+      <div id="region-select-container" className={"col-md-2"}>
         <select
           onChange={handleFilterChangeOnSelect}
           id="region-select"
-          className={`shadow-sm ${theme === 'light' ? 'dark' : 'light'}`}
+          className={`shadow-sm ${theme === "light" ? "dark" : "light"}`}
           aria-label="Search for countries by region."
         >
           <option value="">Filter by Region</option>
